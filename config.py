eefdb6119e8fbe71f0ecc05cc512a324b01ca099
InversTale <jh87776@gmail.com>
2017-10-19 07:37:01 +0900

    test srt2vtt

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-main_sql = ''
-load_sql = ''
-load_detail_sql = ''
-select_anipath_sql = ''
-detail_sql = ''
-insert_detail_sql = ''
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', 
+            password='1111', db='nas', charset='utf8')
+
+main_sql = """insert into anime(title, anime_image) values(%s, %s)"""
+load_sql = """select * from anime"""
+load_detail_sql = """select * from anime_detail where anime_id = %s"""
+select_anipath_sql = """select * from anime_detail where anime_id = %s and episode = %s"""
+detail_sql = """select * from anime where title = %s"""
+insert_detail_sql = """insert into anime_detail(anime_id, episode, file, end_yn) values(%s, %s, %s ,%s)"""
